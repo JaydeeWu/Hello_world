a8261015d6ea12b53865376ebfc410e8b8d39100
WuJingdong <38470998+JaydeeWu@users.noreply.github.com>
2018-04-18 12:35:10 +0800

    Add files via upload

---
+++ helloword.py
@@ -0,0 +1,14 @@
+# this program says hello
+print ("hello world!")
+# Ask the user to input their name and assign it to the name variable
+print("what is your name")
+myName = input()
+# print out greet followed by name
+print("It is good to meet you,"+myName)
+# print out the lenth of the name
+print("the length of your name is"+str(len(myName)))
+# ask for your age and show how old you will be next year
+print("what is your age?")
+myAge=input()
+print("you will be "+str((int(myAge))+1)+" in next year")
+
